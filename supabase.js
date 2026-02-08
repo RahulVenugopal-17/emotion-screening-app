8580bafac384c2fde7e9b8a859ccf9a7e9fb5116
RahulVenugopal-17 <ragul.2203119@srec.ac.in>
2026-02-08 21:56:02 +0530

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,14 +1,8 @@
-// Import Supabase client from CDN (already loaded in index.html)
-
 const SUPABASE_URL = "https://ijrzuqdfpzztufeeqrjs.supabase.co";
 const SUPABASE_KEY = "sb_publishable_gS2Gch12fKV5N4M8qF5eCw_lCdai5xq";
 
-// Create Supabase client
-const supabase = supabaseJs.createClient(
+window.supabase = supabaseJs.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
-
-// Optional: test connection (you can remove later)
-console.log("Supabase connected:", supabase);
 
