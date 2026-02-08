b1da1f35bfa107ae45390149bbfc77cb94203428
RahulVenugopal-17 <ragul.2203119@srec.ac.in>
2026-02-08 23:17:05 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,12 +1,1 @@
 // Supabase configuration
-const SUPABASE_URL = "https://ijrzuqdfpzztufeeqrjs.supabase.co";
-const SUPABASE_KEY = "sb_publishable_gS2Gch12fKV5N4M8qF5eCw_lCdai5xq";
-
-// Create client using Supabase v2 CDN
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
-
-
-
